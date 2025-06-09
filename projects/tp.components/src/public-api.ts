668ea772a1c935c2a1db8db453c8fb423bb610db
gleb <gleb216@gmail.com>
2025-06-09 15:00:29 +0300

    0.0.6 and export rating component in public API

--- projects/tp.components/src/public-api.ts
+++ projects/tp.components/src/public-api.ts
@@ -5,6 +5,7 @@
 export * from './lib/component/audio.player/audio.player.component';
 export * from './lib/component/video.player/video.player.component';
 export * from './lib/component/file.link/file.link.component';
+export * from './lib/component/rating/rating.component';
 export * from './lib/pipe/number.to.time/number.to.time.pipe';
 
 
