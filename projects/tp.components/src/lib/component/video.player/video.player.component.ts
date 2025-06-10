8113a611b515240872d9a657382a5f2d2849aa8d
gleb <gleb216@gmail.com>
2025-06-10 17:19:58 +0300

    0.0.7 update tp-video-player component to include DefaultIconsModule; bump version to

--- projects/tp.components/src/lib/component/video.player/video.player.component.ts
+++ projects/tp.components/src/lib/component/video.player/video.player.component.ts
@@ -1,12 +1,14 @@
 import {booleanAttribute, ChangeDetectionStrategy, Component, input, numberAttribute, output} from '@angular/core';
 import {MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
+import {DefaultIconsModule} from '../../module/default.icons/default.icons.module';
 
 @Component({
   selector: 'tp-video-player',
   imports: [
     MatIconButton,
     MatIcon,
+    DefaultIconsModule
   ],
   templateUrl: './video.player.component.html',
   styleUrl: './video.player.component.scss',
